9a309999e84d7e5e06a5599483f7a2f451bb8a48
Administrator <Administrator@PC-20170518KDMT>
2018-01-28 22:20:01 +0800

    讲课过程中优化

--- Demo/rr_demo.py
+++ Demo/rr_demo.py
@@ -10,7 +10,7 @@ clf.fit(X, y)  # 参数拟合
 
 print("系数w:",clf.coef_)  # 系数
 print("常量b:",clf.intercept_)  # 常量
-print("预测属性为[3,3]的值:",clf.predict([[3, 3]]))  # 求预测值
+print("预测属性为[5,6]的值:",clf.predict([[5, 6]]))  # 求预测值
 print("R^拟合优度:",clf.score(X, y))  # R^2，拟合优度
 print("参数信息:",clf.get_params())  # 获取参数信息
 print("重新设置参数:",clf.set_params(fit_intercept=False))  # 重新设置参数
